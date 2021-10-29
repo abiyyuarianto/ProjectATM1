f41813ecbefa9b744e492cdde22c0d585e3b030c
Muhammad Abiyyu Aly Arianto <aly.ariannto@gmal.com>
2021-10-29 20:45:50 +0700

    Perbaikan main dari doni (Try Catch)

--- src/ATMProject/Main.java
+++ src/ATMProject/Main.java
@@ -1,6 +1,7 @@
 package ATMProject;
 import java.math.BigDecimal;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
@@ -13,10 +14,9 @@ public class Main {
         DataBank dataBank = new DataBank();
         DispenserUangTunai dispenserUangTunai = new DispenserUangTunai();
 
-        System.out.print("Masukan No Rekening Anda: ");
-        noRek = input.nextInt();
-
         do{
+            System.out.print("Masukan No Rekening Anda: ");
+            noRek = input.nextInt();
             System.out.print("Masukan PIN Anda: ");
             int PIN = input.nextInt();
             if(dataBank.authenticateUser(noRek,PIN)){
@@ -27,34 +27,51 @@ public class Main {
         } while (!userAuthenticate);
 
         do {
-                System.out.println("\t=======Menu=======");
-                System.out.println("\t1.Cek Saldo");
-                System.out.println("\t2.Tarik Tunai");
-                System.out.println("\t3.Setor Tunai");
-                System.out.println("\t4.Keluar");
-                System.out.print("Pilih Menu: ");
+            System.out.println("\t=======Menu=======");
+            System.out.println("\t1.Cek Saldo");
+            System.out.println("\t2.Tarik Tunai");
+            System.out.println("\t3.Setor Tunai");
+            System.out.println("\t4.Keluar");
+            System.out.print("Pilih Menu: ");
 
-                menu = input.nextInt();
-                switch (menu) {
-                    case 1:
-                        PengecekanSaldo inquiry = new PengecekanSaldo(dataBank);
-                        inquiry.execute();
-                        break;
-                    case 2:
-                        System.out.print("Jumlah yang ditarik = ");
-                        BigDecimal cash = input.nextBigDecimal();
-                        PenarikanTunai tarikTunai = new PenarikanTunai(cash, dataBank,dispenserUangTunai);
-                        tarikTunai.execute();
-                        break;
-                    case 3:
-                        System.out.print("Jumlah yang disetor = ");
-                        BigDecimal cashSetor = input.nextBigDecimal();
-                        PenyetoranTunai setorTunai = new PenyetoranTunai(cashSetor, dataBank, dispenserUangTunai);
-                        setorTunai.execute();
-                        break;
-                    case 4:
-                        break;
+            menu = input.nextInt();
+            if (menu == 1){
+                PengecekanSaldo inquiry = new PengecekanSaldo(dataBank);
+                inquiry.execute();
+                System.out.println("=======================");
+            }
+            else if (menu == 2){
+                try {
+                    System.out.print("Jumlah yang ditarik = ");
+                    BigDecimal cash = input.nextBigDecimal();
+                    PenarikanTunai tarikTunai = new PenarikanTunai(cash, dataBank,dispenserUangTunai);
+                    tarikTunai.execute();
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid Input");
+                    System.out.println("==========================");
+                    input.next();
                 }
+            }
+            else if (menu == 3){
+                try {
+                    System.out.print("Jumlah yang disetor = ");
+                    BigDecimal cashSetor = input.nextBigDecimal();
+                    PenyetoranTunai setorTunai = new PenyetoranTunai(cashSetor, dataBank, dispenserUangTunai);
+                    setorTunai.execute();
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid Input");
+                    System.out.println("==========================");
+                    input.next();
+                }
+            }
+            else if (menu == 4) {
+                break;
+            }
+            else {
+                System.out.println("Invalid Input");
+                System.out.println("==========================");
+                //break;
+            }
         }while (menu!=4);
 
     }
